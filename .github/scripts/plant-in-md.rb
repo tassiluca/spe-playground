8c575eb95b7b4b948d29cfc3f08a3a7c29280823
Luca Tassinari <luca.tassinari.2000@gmail.com>
2024-07-09 12:36:27 +0200

    ci: add test for checkout action

--- .github/scripts/plant-in-md.rb
+++ .github/scripts/plant-in-md.rb
@@ -1,4 +1,5 @@
 require 'find'
+require 'fileutils'
 
 def plantuml_installed?
   system("command -v plantuml > /dev/null 2>&1")
@@ -9,7 +10,7 @@ unless plantuml_installed?
   exit 1
 end
 
-if ARGV.length != 1 || !File.directory?(ARGV[0])
+if ARGV.length != 2 || !File.directory?(ARGV[0]) || !File.directory?(ARGV[1])
   puts "A script to embed PlantUML diagrams in markdown files."
   puts ""
   puts "Usage: plant-in-md.rb <directory>"
@@ -19,29 +20,34 @@ if ARGV.length != 1 || !File.directory?(ARGV[0])
   exit 1
 end
 
-def generate_svg_diagrams(path)
+def generate_svg_diagrams(path, output_dir)
   system("plantuml -tsvg #{path}")
+  Dir.glob(File.join(File.dirname(path), "*.svg")).each do |file|
+    FileUtils.mv(file, output_dir)
+  end
 end
 
-def update_md(path)
+def update_md(path, output_dir)
   content = File.read(path)
   updated_content = content.gsub(/```plantuml\n(@start\w+)\s+(\w+)\n(.*?)\n```/m) do
     diagram_name = $2
-    "![#{diagram_name} plantuml diagram](#{diagram_name}.svg)"
+    puts File.join(output_dir, diagram_name)
+    "![#{diagram_name} plantuml diagram](/#{File.join(output_dir, diagram_name)}.svg)"
   end
   File.write(path, updated_content)
 end
 
-def process_md_files(directory)
+def process_md_files(directory, output_dir)
   Find.find(directory) do |path|
     if File.file?(path) && File.extname(path) == ".md"
-      generate_svg_diagrams(path)
-      update_md(path)
+      generate_svg_diagrams(path, output_dir)
+      update_md(path, output_dir)
     end
   end
 end
 
 root_directory = ARGV[0]
+output_directory = ARGV[1]
 puts "Processing markdown files in #{root_directory}..."
-process_md_files(root_directory)
+process_md_files(root_directory, output_directory)
 puts "Done."
--- plant-in-md/wbs.md
+++ plant-in-md/wbs.md
@@ -1,28 +1,5 @@
 # WBS
 
-```plantuml
-@startwbs wbs
-
-* PositionPal.
-
-** Organizzazione, configurazione e \n inizializzazione dei workspace
-*** Configurazione build tool
-*** Configurazione pipeline di CI
-*** Configurazione pipeline di CD
-*** Configurazione bot per aggiornamento \n automatico delle dipendenze
-*** Configurazione bot per il merge \n automatizzato di Pull Request
-**** Definizione delle regole di merge automatico
-*** Configurazione di un sistema di ticketing automatizzato
-
-** Design phase
-*** Model of AsIs Processes Completed
-**** Model of AsIs Processes Completed1
-**** Model of AsIs Processes Completed2
-*** Measure AsIs performance metrics
-*** Identify Quick Wins
-** Complete innovate phase
-
-@endwbs
-```
+![wbs plantuml diagram](/res/wbs.svg)
 
 Hello World
